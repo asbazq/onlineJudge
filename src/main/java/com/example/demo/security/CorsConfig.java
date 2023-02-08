8530c25c534ba9ff5ccf46800d341f1c4bcf71fc
LAPTOP-4RG8174M\User <wjdgns5488@naver.com>
2023-02-08 18:08:29 +0900

    [Cors import변경]

--- src/main/java/com/example/demo/security/CorsConfig.java
+++ src/main/java/com/example/demo/security/CorsConfig.java
@@ -1,10 +1,10 @@
 package com.example.demo.security;
 
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import com.example.demo.security.jwt.JwtProperties;
 
