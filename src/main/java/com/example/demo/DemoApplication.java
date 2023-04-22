ec467a85b2b2605ec0c403867bceb9cf870dc40b
LAPTOP-4RG8174M\User <wjdgns5488@naver.com>
2023-04-23 04:20:44 +0900

    feat : entity 작성, 수정 시간 추가

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,8 +3,10 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DemoApplication {
 
@@ -12,7 +14,7 @@ public class DemoApplication {
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
