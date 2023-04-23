6498f9a94e3940746f12516c90fded8bc4e6f3ac
LAPTOP-4RG8174M\User <wjdgns5488@naver.com>
2023-04-23 19:11:59 +0900

    style

--- src/main/java/com/example/demo/controller/SearchController.java
+++ src/main/java/com/example/demo/controller/SearchController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +18,9 @@ import lombok.RequiredArgsConstructor;
 public class SearchController {
     private final SearchService searchService;
 
+    // 검색
     @GetMapping("api/search")
-    public ResponseEntity<List<QuestionResponseDto>> search(@RequestParam("query") String query, Model model) {
+    public ResponseEntity<List<QuestionResponseDto>> search(@RequestParam("query") String query) {
         // @ReuqestParam 어노테이션은 HttpServletRequest 객체와 같은 역할
         List<QuestionResponseDto> dto = searchService.search(query);
         return ResponseEntity.status(HttpStatus.OK).body(dto);
