5e9f112251bc0e4e199d37e5089e8b95f725325d
asbazq <107836678+asbazq@users.noreply.github.com>
2023-05-17 22:20:48 +0900

    Update converter.sh

--- converter.sh
+++ converter.sh
@@ -1,23 +1,46 @@
-#!/bin/sh
+#!/bin/bash
 
 filename=${1%.*}
 extension=${1##*.}
 language=$2
-param=$3
+params="$3"
 
+# check if the file exists
+if [ ! -f "$1" ]; then
+        echo "File not fond: $1"
+        exit 1
+fi
+
+# parse the String
+IFS=',' read -ra args <<< "$params"
+
+
+# Check program language
 case $language in
+
 	c)
-		gcc -o $filename $1
-		su -c "./$filename $param" dummy
-		rm $filename
-	;;
-	java)
-		javac $1
-		su -c "java $filename $param" dummy
-		rm "${filename}.class"
-	;;
-	python)
-		su -c "python3 $1 $param" dummy
-	;;
+		#compile and execute the c file
+                gcc -o "$filename" "$1"
+                su -c "./$filename ${args[@]}" dummy
+                exit
+        ;;
+        java)
+                #compile and execute the java file
+                javac "$1"
+                su -c "java $filename ${args[@]}" dummy
+                exit
+        ;;
+        python)
+                #compile and execute the python file
+		  su -c "python3 $1 ${args[@]}" dummy
+                exit
+        ;;
+                *)
+                echo "Invalid language: $language"
+                exit 1
+        ;;
 esac
 
+
+
+                                                            
