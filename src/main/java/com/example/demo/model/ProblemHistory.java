57f3414229f66bfeb9c6ab8f472bdbd97c32e83e
LAPTOP-4RG8174M\User <wjdgns5488@naver.com>
2023-08-02 21:30:19 +0900

    fix : 기본 생성자 추가

--- src/main/java/com/example/demo/model/ProblemHistory.java
+++ src/main/java/com/example/demo/model/ProblemHistory.java
@@ -10,12 +10,16 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProblemHistory extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
